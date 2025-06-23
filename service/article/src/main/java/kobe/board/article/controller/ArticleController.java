5d4c1810ebfa3223be2b8264237e29cc6cba008e
Kobe <dev.skyachieve91@gmail.com>
2025-06-24 08:56:19 +0900

    feat: ArticleController CRUD 생성 및 구현.

--- service/article/src/main/java/kobe/board/article/controller/ArticleController.java
+++ service/article/src/main/java/kobe/board/article/controller/ArticleController.java
@@ -11,4 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class ArticleController {
 	private final ArticleService articleService;
 
+
+	@GetMapping("/{articleId}")
+	public ArticleResponse read(@PathVariable Long articleId) {
+		return articleService.read(articleId);
+	}
+
+	@PostMapping
+	public ArticleResponse create(@RequestBody ArticleCreateRequest request) {
+		return articleService.create(request);
+	}
+
+	@PutMapping("/{articleId}")
+	public ArticleResponse update(@PathVariable Long articleId, @RequestBody ArticleUpdateRequest request) {
+		return articleService.update(articleId, request);
+	}
+
+	@DeleteMapping("/{articleId}")
+	public void delete(@PathVariable Long articleId) {
+		articleService.delete(articleId);
+	}
 }
