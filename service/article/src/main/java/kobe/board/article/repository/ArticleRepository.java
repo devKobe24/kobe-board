df18ed16b444f8b053a7c8f95f17f3c1e2a5e656
Kobe <dev.skyachieve91@gmail.com>
2025-06-24 08:58:21 +0900

    feat: added Repository annotation

--- service/article/src/main/java/kobe/board/article/repository/ArticleRepository.java
+++ service/article/src/main/java/kobe/board/article/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package kobe.board.article.repository;
 import kobe.board.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
