9d8292c7ff3a79a0691fc88bf18dbb5bd36b6812
Kobe <dev.skyachieve91@gmail.com>
2025-06-24 08:58:41 +0900

    feat: import Repository dependency

--- service/article/src/main/java/kobe/board/article/repository/ArticleRepository.java
+++ service/article/src/main/java/kobe/board/article/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package kobe.board.article.repository;
 
 import kobe.board.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
