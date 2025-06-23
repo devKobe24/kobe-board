42d235d7a434dd8bfc9802dbd6285b4669600931
Kobe <dev.skyachieve91@gmail.com>
2025-06-24 06:43:21 +0900

    Article entity

---
+++ service/article/src/main/java/kobe/board/article/entity/Article.java
@@ -0,0 +1,46 @@
+package kobe.board.article.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Table(name = "article")
+@Getter
+@Entity
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+	@Id
+	private Long articleId;
+	private String title;
+	private String content;
+	private Long boardId; // shard key
+	private Long writerId;
+	private LocalDateTime createTime;
+	private LocalDateTime modifiedAt;
+
+	public static Article create(Long articleId, String title, String content, Long boardId, Long writerId) {
+		Article article = new Article();
+		article.articleId = articleId;
+		article.title = title;
+		article.content = content;
+		article.boardId = boardId;
+		article.writerId = writerId;
+		article.createTime = LocalDateTime.now();
+		article.modifiedAt = article.createTime;
+		return article;
+	}
+
+	public void update(String title, String content) {
+		this.title = title;
+		this.content = content;
+		modifiedAt = LocalDateTime.now();
+	}
+}
