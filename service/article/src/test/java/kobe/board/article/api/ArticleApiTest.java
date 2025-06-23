36ed99bb2b5472fbb2091b4b6092ac9fb5d2308d
Kobe <dev.skyachieve91@gmail.com>
2025-06-24 08:54:54 +0900

    test: Article CRUD test 성공

--- service/article/src/test/java/kobe/board/article/api/ArticleApiTest.java
+++ service/article/src/test/java/kobe/board/article/api/ArticleApiTest.java
@@ -1,4 +1,76 @@
 package kobe.board.article.api;
 
 public class ArticleApiTest {
+	RestClient restClient = RestClient.create("http://localhost:9000");
+
+	@Test
+	void createTest() {
+		ArticleResponse response = create(
+			new ArticleCreateRequest(
+				"Test Title",
+				"Test Content",
+				1L, // writerId
+				1L  // boardId
+			)
+		);
+		System.out.println("[CREATE] response = " + response);
+	}
+
+	@Test
+	void readTest() {
+		ArticleResponse response = read(195672956478894080L);
+		System.out.println("[READ] response = " + response);
+	}
+
+	ArticleResponse read(Long articleId) {
+		return restClient.get()
+			.uri("/v1/articles/{articleId}", articleId)
+			.retrieve()
+			.body(ArticleResponse.class);
+	}
+
+	@Test
+	void updateTest() {
+		update(195672956478894080L);
+		ArticleResponse response = read(195672956478894080L);
+		System.out.println("[UPDATE] response = " + response);
+	}
+
+	void update(Long articleId) {
+		restClient.put()
+			.uri("/v1/articles/{articleId}", articleId)
+			.body(new ArticleUpdateRequest("Updated Title", "Updated Content"))
+			.retrieve();
+	}
+
+	@Test
+	void deleteTest() {
+		restClient.delete()
+			.uri("/v1/articles/{articleId}", 195672956478894080L)
+			.retrieve();
+	}
+
+	ArticleResponse create(ArticleCreateRequest request) {
+		return restClient.post()
+			.uri("/v1/articles")
+			.body(request)
+			.retrieve()
+			.body(ArticleResponse.class);
+	}
+
+	@Getter
+	@AllArgsConstructor
+	static class ArticleCreateRequest {
+		private String title;
+		private String content;
+		private Long writerId;
+		private Long boardId;
+	}
+
+	@Getter
+	@AllArgsConstructor
+	static class ArticleUpdateRequest {
+		private String title;
+		private String content;
+	}
 }
