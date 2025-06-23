9eb6c3b485afb9a89762180a6148bc717bf51f91
Kobe <dev.skyachieve91@gmail.com>
2025-06-24 06:44:12 +0900

    ArticleResponse

---
+++ service/article/src/main/java/kobe/board/article/service/response/ArticleResponse.java
@@ -0,0 +1,32 @@
+package kobe.board.article.service.response;
+
+import jakarta.persistence.Id;
+import kobe.board.article.entity.Article;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+public class ArticleResponse {
+	private Long articleId;
+	private String title;
+	private String content;
+	private Long boardId;
+	private Long writerId;
+	private LocalDateTime createTime;
+	private LocalDateTime modifiedAt;
+
+	public static ArticleResponse from(Article article) {
+		ArticleResponse response = new ArticleResponse();
+		response.articleId = article.getArticleId();
+		response.title = article.getTitle();
+		response.content = article.getContent();
+		response.boardId = article.getBoardId();
+		response.writerId = article.getWriterId();
+		response.createTime = article.getCreateTime();
+		response.modifiedAt = article.getModifiedAt();
+		return response;
+	}
+}
