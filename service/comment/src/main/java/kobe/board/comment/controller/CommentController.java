60b121c9d2e7bd7456631db2ddee8286b7254840
Kobe <dev.skyachieve91@gmail.com>
2025-06-26 07:30:54 +0900

    feat: CommentController CRD 생성 및 구현.
    - create API
    - read API
    - delete API

--- service/comment/src/main/java/kobe/board/comment/controller/CommentController.java
+++ service/comment/src/main/java/kobe/board/comment/controller/CommentController.java
@@ -1,9 +1,30 @@
 package kobe.board.comment.controller;
 
+import kobe.board.comment.service.CommentService;
+import kobe.board.comment.service.request.CommentCreateRequest;
+import kobe.board.comment.service.response.CommentResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/v1/comments")
 @RequiredArgsConstructor
 public class CommentController {
+
+	private final CommentService commentService;
+
+	@GetMapping("/{commentId}")
+	public CommentResponse read(@PathVariable("commentId") Long commentId) {
+		return commentService.read(commentId);
+	}
+
+	@PostMapping
+	public CommentResponse create(@RequestBody CommentCreateRequest request) {
+		return commentService.create(request);
+	}
+
+	@DeleteMapping("/{commentId}")
+	public void delete(@PathVariable("commentId") Long commentId) {
+		commentService.delete(commentId);
+	}
 }
