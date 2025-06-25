86125233cba44d34b1dd32cfaec12d34d6dbf243
Kobe <dev.skyachieve91@gmail.com>
2025-06-26 07:23:53 +0900

    feat: CommentController init.

---
+++ service/comment/src/main/java/kobe/board/comment/controller/CommentController.java
@@ -0,0 +1,9 @@
+package kobe.board.comment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+}
