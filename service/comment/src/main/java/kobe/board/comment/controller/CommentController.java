585f860c480872dd58c1251417a06f7f5e660ec5
Kobe <dev.skyachieve91@gmail.com>
2025-06-27 05:55:38 +0900

    feat: readAll API 생성 및 구현 2가지.
    - "/v1/comments" :GET
    - "/v1/comments/infinite-scroll": GET

--- service/comment/src/main/java/kobe/board/comment/controller/CommentController.java
+++ service/comment/src/main/java/kobe/board/comment/controller/CommentController.java
@@ -2,9 +2,12 @@ package kobe.board.comment.controller;
 
 import kobe.board.comment.service.CommentService;
 import kobe.board.comment.service.request.CommentCreateRequest;
+import kobe.board.comment.service.response.CommentPageResponse;
 import kobe.board.comment.service.response.CommentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/comments")
@@ -26,5 +29,24 @@ public class CommentController {
 	@DeleteMapping("/{commentId}")
 	public void delete(@PathVariable("commentId") Long commentId) {
 		commentService.delete(commentId);
+	}
+
+	@GetMapping
+	public CommentPageResponse readAll(
+		@RequestParam("articleId") Long articleId,
+		@RequestParam("page") Long page,
+		@RequestParam("pageSize") Long pageSize
+		) {
+		return commentService.readAll(articleId, page, pageSize);
+	}
+
+	@GetMapping("/infinite-scroll")
+	public List<CommentResponse> readAll(
+		@RequestParam("articleId") Long articleId,
+		@RequestParam(value = "lastParentCommentId", required = false) Long lastParentCommentId,
+		@RequestParam(value = "lastCommentId", required = false) Long lastCommentId,
+		@RequestParam("pageSize") Long pageSize
+	) {
+		return commentService.readAll(articleId, lastParentCommentId, lastCommentId, pageSize);
 	}
 }
