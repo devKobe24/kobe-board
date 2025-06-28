49fd7283d3be62b9c0588abf8030320a9a5026b5
Kobe <dev.skyachieve91@gmail.com>
2025-06-28 11:51:09 +0900

    feat: ArticleLikeRepository 인터페이스 생성 및 구성 그리고 구현.
    - findByArticleIdAndUserId(Long articleId, Long userId) 구현.

---
+++ service/like/src/main/java/kobe/board/like/repository/ArticleLikeRepository.java
@@ -0,0 +1,12 @@
+package kobe.board.like.repository;
+
+import kobe.board.like.entity.ArticleLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+	Optional<ArticleLike> findByArticleIdAndUserId(Long articleId, Long userId);
+}
