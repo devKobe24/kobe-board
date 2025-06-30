72b6ffb882a2b5c328631b96c460855f21215393
Kobe <dev.skyachieve91@gmail.com>
2025-07-01 05:50:44 +0900

    feat: ArticleViewController 생성 및 구현.
    - increase API 생성 및 구현
    - count API 생성 및 구현

---
+++ service/view/src/main/java/kobe/board/view/controller/ArticleViewController.java
@@ -0,0 +1,25 @@
+package kobe.board.view.controller;
+
+import kobe.board.view.service.ArticleViewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/article-views")
+public class ArticleViewController {
+	private final ArticleViewService articleViewService;
+
+	@PostMapping("/articles/{articleId}/users/{userId}")
+	public Long increase(
+		@PathVariable("articleId") Long articleId,
+		@PathVariable("userId") Long userId
+	) {
+		return articleViewService.increase(articleId, userId);
+	}
+
+	@GetMapping("/articles/{articleId}/count")
+	public Long count(@PathVariable Long articleId) {
+		return articleViewService.count(articleId);
+	}
+}
