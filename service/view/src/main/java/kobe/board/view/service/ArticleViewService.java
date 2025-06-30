7daea0f5cb54d6d3da1d5828ec30449ecacaff03
Kobe <dev.skyachieve91@gmail.com>
2025-07-01 06:58:45 +0900

    fix: increase 메서드 내부 로직 수정
    - 조회수 어뷰징 방지 정책 로직 추가.

--- service/view/src/main/java/kobe/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/kobe/board/view/service/ArticleViewService.java
@@ -1,17 +1,27 @@
 package kobe.board.view.service;
 
 import kobe.board.view.repository.ArticleViewCountRepository;
+import kobe.board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
 	private final ArticleViewCountRepository articleViewCountRepository;
 	private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+	private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
 	private static final int BACK_UP_BACH_SIZE = 100;
+	private static final Duration TTL = Duration.ofMinutes(10);
 
 	public Long increase(Long articleId, Long userId) {
+		if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+			return articleViewCountRepository.read(articleId);
+		}
+
 		Long count = articleViewCountRepository.increase(articleId);
 		System.out.println("[INCREASE] articleId = " + articleId + ", count = " + count);
 		if (count % BACK_UP_BACH_SIZE == 0) {
